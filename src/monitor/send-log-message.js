d7e1165787a1e908480328bba090cdde8b40c74e
Shannon Lal <shannonlal@hotmail.com>
2017-09-27 20:38:52 -0400

    Check in first tests for send message

--- src/monitor/send-log-message.js
+++ src/monitor/send-log-message.js
@@ -3,26 +3,30 @@ var AWS = require('aws-sdk');
 var accessKey = process.env.AWS_ACCESS_KEY;
 var secretKey = process.env.AWS_SECRET_KEY;
 var DeliveryStreamName = process.env.AWS_FIREHOSE_DELIVERY_STREAM_NAME;
+var region = process.env.AWS_REGION;
 
 var firehose = new AWS.Firehose(
     {apiVersion: '2015-08-04',
     accessKeyId:accessKey,
-    secretAccessKey:secretKey
+    secretAccessKey:secretKey,
+    region:region
     }
 );
 
 
 var params = {
-    DeliveryStreamName: 'STRING_VALUE', /* required */
-    Records: [ /* required */
+    DeliveryStreamName: DeliveryStreamName,
+    Records: [ 
       {
-        Data: new Buffer('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */ /* required */
+        Data:'STRING_VALUE 1' 
       },
-      /* more items */
+      {
+        Data:'STRING_VALUE 2' 
+      }
     ]
   };
 
-  firehose.putRecordBatch(params, function(err, data) {
+firehose.putRecordBatch(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else     console.log(data);           // successful response
   });
