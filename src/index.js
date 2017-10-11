e2f4f9f0f08718cab8b663683f536bf8b3ae569d
Shannon Lal <shannonlal@hotmail.com>
2017-10-10 20:45:04 -0400

    Put in first version of code

--- src/index.js
+++ src/index.js
@@ -13,3 +13,17 @@ var options = {
 
 tailFile.tailFile( './test.txt', logQueue, options);
 
+//TODO Test this tomorrow and integrate
+var processLogs = function(){
+    //TODO Replace this tomorrow
+    var startTimer = new Date();
+
+    while( starTime > 5000 && !logQueue.isEmpty()){
+        console.log( 'Message', logQueue.pop());
+    }
+}
+
+var interval = setInterval(function() {
+    console.log(str1 + " " + str2);
+  }, 10000, "Hello.", "How are you?");
+  
