c243ccf310c3192123262adf93387b78ec87ad37
Shannon Lal <shannonlal@hotmail.com>
2017-10-10 20:34:02 -0400

    Check in latest test case

--- test/log-queue-spec.js
+++ test/log-queue-spec.js
@@ -21,11 +21,41 @@ describe('should tests log-queue to ensure proper operation', function () {
 
         logQueue.push('test messge');
 
-        console.log( 'queue', logQueue);
+        //console.log( 'queue', logQueue);
         assert.ok( !logQueue.isEmpty());
         let rst = logQueue.pop();
 
         assert.equal( rst, 'test messge');
+
+        done();
+      });
+
+      it('Should verify queue empty with nothing added', function ( done ){
+        assert.notEqual(LogQueue, undefined);
+
+        var logQueue = new LogQueue();
+
+        assert.notEqual(logQueue, undefined);
+
+        assert.ok( logQueue.isEmpty());
+
+        done();
+      });
+
+      it('Should add a message, remove and verify it is empty', function ( done ){
+        assert.notEqual(LogQueue, undefined);
+
+        var logQueue = new LogQueue();
+
+        assert.notEqual(logQueue, undefined);
+
+        logQueue.push('test messge');
+
+        assert.ok( !logQueue.isEmpty());
+
+        let rst = logQueue.pop();
+        //console.log( 'queue', logQueue);
+        assert.ok( logQueue.isEmpty());
 
         done();
       });
